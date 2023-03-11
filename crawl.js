31b9f4419772971201da8104605ccae4298c2ce3
anubhavdubey13 <anubhav.dubey.stats@gmail.com>
2023-03-11 13:55:40 +0530

    finished normalizeURL function

--- crawl.js
+++ crawl.js
@@ -1,5 +1,10 @@
 function normalizeURL(url) {
-    return url
+    const urlObj = new URL(url)
+    let normURL = `${urlObj.hostname}${urlObj.pathname}`
+    if (normURL.length > 0 && normURL.slice(-1) === '/') {
+        normURL = normURL.slice(0, -1)
+    }
+    return normURL
 }
 
 module.exports = {
