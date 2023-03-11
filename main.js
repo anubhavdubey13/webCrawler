ecb79473d9f0b0d825b760601b27ba5bdfd81661
anubhavdubey13 <anubhav.dubey.stats@gmail.com>
2023-03-11 16:16:53 +0530

    Main function to accept just one input

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+function main() {
+    const totalInputs = process.argv.length - 2
+
+    if (totalInputs < 1) {
+        console.log('No input provided')
+        return
+    } else if (totalInputs > 1) {
+        console.log(`${totalInputs} inputs provided. Need just one`)
+        return
+    }
+    const baseURL = process.argv[2]
+    console.log(`started crawling at ${baseURL}`)
+}
+
+main()
